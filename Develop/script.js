e2618e5a8d28afaf4d77914fe91fa1992c044e6a
efrainrf <efrainburner22@gmail.com>
2024-03-06 20:43:41 -0800

    added notes on how to get started on logic

--- Develop/script.js
+++ Develop/script.js
@@ -21,3 +21,139 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+ 
+//notes below:
+/*
+//global dom elements
+var timeDisplayEl = $("#time-display");
+var projectDisplayEl = $("project-display");
+var projectFormEl = $("project-form");
+var projectNameInputEl = $("#project-name-input");
+var projectNameTypeEl = $("#project-type-input");
+var projectNameDateEl = $("#project-date-input");
+
+//display the time
+function diplayTime() {
+    var rightNow  = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+} 
+
+// reads projects from local storage
+// returns an empty array if there arent any projects 
+function readProjectsFromStorage() {
+    var projects = localStorage.getItem('projects');
+    if (projects) {
+        projects = JSON.parse(projects);
+    } else {
+        projects = [];
+    }
+    return projects;
+}
+
+//takes an array of projects and saves them in localStorage
+function SaveProjectsToStorage(projects) {
+localStorage.setItem('projects', JSON.stringify(projects));
+}
+
+
+//gets project data from local storage
+function printProjectData() {
+//clear current projects
+    projectDisplayEl.empty();
+//get projects from localStorage
+    var projects = readProjectsFromStorage();
+    //projects will look like this {[{projectname:name,date:1999},{projectname:name}]}
+    // ALWAYS CONSOLE ANYTHING THAT IS CONFUSING
+//loop through each project and create a row
+for (var i = 0; i < projects.length; i+= 1) {
+var project = projects[i];
+var projectDate = dayjs(project.date);
+// get date/time for start of today
+var today = dayjs().startOf('day');
+// create row and columns for project
+var rowEl = $('<tr>');
+var nameEl = $('<td>').text(project.name);
+var typeEl = $('<td>').text(project.type);
+var dateEl = $('<td>').text(projectDate.format('MM/DD/YYYY'));
+
+// save the index of the project as a data attribute on the button. This will
+//be used when removing the project from an array
+var deleteEl = $(
+'<td><button class="bton btn-sm btn-delete-project" data-index="' + i +
+'">X</button></td>'
+);
+
+// add calss to row by comparing project date to today's date
+if (projectDate.isBefore(today)) {
+    rowEl.addClass('project-late');
+}else if (projectDate.isSame(today)) {
+    rowEl.addClass('project-today');
+}
+//append elements to DOM to display them
+rowEl.append(nameEl, typeEl, dateEl, deleteEl);
+projectDisplayEl.append(rowEl);
+
+}}
+//removes a project from local storage and prints the project data
+function handleDeleteProject() {
+    var projectIndex = parseInt($(this).attr('data-index'));
+    var projects = readProjectsFromStorage();
+    //remove project from the array
+    projects.splice(projectIndex, 1);
+    SaveProjectsToStorage(projects);
+
+    //print projects
+    printProjectData();
+}
+
+
+//add a project to local storage and prints the project data
+function handleProjectFormSubmit(event) {
+    event.preventDefault();
+    // reader user input from the form
+    var projectName = projectNameInputEl.val().trim();
+    var projectType = projectNameTypeEl.val();
+    var projectDate = projectNameDateEl.val();
+
+    var newProject = {
+        name: projectName,
+        type: projectType,
+        date: projectDate,
+    };
+
+    //add project to local storage
+    var projects = readProjectsFromStorage();
+    projects.push(newProject);
+    SaveProjectsToStorage(projects);
+    // print project data
+    printProjectData();
+    //clear the form inputs
+    projectNameInputEl.val('');
+    projectNameTypeEl.val('');
+    projectNameDateEl.val('');
+}
+
+projectFormEl.on('submit', handleProjectFormSubmit);
+// use jquery event delegation to listen for clicks on dynamically added delete
+projectDisplayEl.on('click', 'btn-delete-project', handleDeleteProject);
+
+diplayTime();
+setInterval(diplayTime, 1000);
+printProjectData();
+
+/*
+function updatehour() {
+const currentTime = dayjs()
+const updateTime = currentTime.format('h:mm A')
+$('#currentDay').textContent = updateTime;
+}
+setInterval(updatehour, 1000);
+
+updatehour();
+*/
+
+//make a table tag
+// make three trs
+//make three ths
+//append all three ths to the tr
+//append the tr to the table
